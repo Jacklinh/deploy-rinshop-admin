98835592122020881b5a2adeff2001df5f2d1395
Jacklinh <jacklinh79@gmail.com>
2024-12-14 22:58:15 +0700

    update

--- src/constants/configs.ts
+++ src/constants/configs.ts
@@ -1,4 +1,4 @@
 export const globalSetting = {
-    URL_API: 'https://deploy-rinshop-restfulapi-mongoose-be4ge65gk.vercel.app/api/v1',
-    UPLOAD_DIRECTORY: 'https://deploy-rinshop-restfulapi-mongoose-be4ge65gk.vercel.app/'
+    URL_API: 'https://deploy-rinshop-restfulapi-mongoose-92zgvd0de.vercel.app/api/v1',
+    UPLOAD_DIRECTORY: 'https://deploy-rinshop-restfulapi-mongoose-92zgvd0de.vercel.app/'
 }
